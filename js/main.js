539bb47d84d6f54087593d3e325cd41f0fccae7c
shinji0320 <shinji.blue.one@gmail.com>
2021-02-27 06:36:16 +0900

    Like_countUp_component

--- js/main.js
+++ js/main.js
@@ -59,4 +59,24 @@
       }
     }
   });
+
+  var likeComponent =Vue.extend({
+    data: function(){
+      return{
+        count: 0
+      }
+    },
+    template: '<button @click="countUp">Like {{ count }}</button>',
+    methods: {
+      countUp: function(){
+        this.count++;
+      }
+    }
+  });
+  var app = new Vue({
+    el: '#app3',
+    components: {
+      'like-component': likeComponent
+    }
+  });
 })();
